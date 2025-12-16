d1dbc2c8b5f0debabe2b54f3437c39436c4e54d1
OurStoRi <dddd3083@gmail.com>
2025-12-17 00:42:30 +0800

    Fix: Configure Vite for multi-page app (include team.html in build)

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  base: './',
+    base: './',
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                team: resolve(__dirname, 'team.html'),
+            },
+        },
+    },
 })
